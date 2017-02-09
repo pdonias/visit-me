c31eb3c82ab1c9827df36585b47e2ca4271cfc50
laurentfite <laurent.fite@gmail.com>
2017-02-09 19:10:41 +0100

    changed database link and constants to push on remote

--- website/config/database.js
+++ website/config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
   'secret': 'devdacticIsAwesome',
-  'database': 'mongodb://localhost/visit'
+  //'database': 'mongodb://localhost/visit'
+  'database': 'mongodb://<dbuser>:<dbpassword>@ds047945.mlab.com:47945/heroku_3s1nw3tq'
 };
