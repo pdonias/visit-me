44c359bdafff52a179c4293be0176761c02f76b7
laurentfite <laurent.fite@gmail.com>
2017-02-10 13:22:04 +0100

    Removed chrome extension

--- public/js/constants.js
+++ public/js/constants.js
@@ -5,6 +5,6 @@ angular.module('veasit.constants', [])
 })
 
 .constant('API_ENDPOINT', {
-  //url: 'http://localhost:3000/api' // local
-  url: 'https://veasit.herokuapp.com/api' // remote
+  url: 'http://localhost:3000/api' // local
+  //url: 'https://veasit.herokuapp.com/api' // remote
 });
--- website/config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'secret': 'devdacticIsAwesome',
-  //'database': 'mongodb://localhost/visit'
-  'database': 'mongodb://laurentf:veasitaflat!@ds047945.mlab.com:47945/heroku_3s1nw3tq'
-};
