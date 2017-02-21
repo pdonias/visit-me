c52dcaad0c9a770dac6906e30dc0a16299f74075
laurentfite <laurent.fite@gmail.com>
2017-02-21 14:22:02 +0100

    Changing list page when no entries

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,6 +2,15 @@ angular.module('veasit.controllers', ['veasit.constants'])
 
 .controller('ListController', function($scope, $http, $location, ngProgressFactory, API_ENDPOINT) {
 
+  $scope.readableWebsites = [
+    {url: "", name: "", img: ""},
+    {url: "", name: "", img: ""},
+    {url: "", name: "", img: ""},
+    {url: "", name: "", img: ""},
+    {url: "", name: "", img: ""},
+    {url: "", name: "", img: ""},
+  ];
+
   $scope.emailFormat = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
   $scope.linkFormat = /https?:\/\/(www\.)?(fnaim38|leboncoin|avendrealouer|paruvendu|pap|seloger)\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/;
 
