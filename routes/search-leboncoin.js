f222792f236e25a35ca57958f037c4d0f44a46ae
laurentfite <laurent.fite@gmail.com>
2017-03-09 16:59:36 +0100

    Search from Veasit
    - Searching from Veasit now fetches results from LBC & SL
    - Adding an entry from SL works (LBC doesnt)

--- routes/search-leboncoin.js
+++ routes/search-leboncoin.js
@@ -1,6 +1,8 @@
-let cheerio = require('cheerio')
-let jsonframe = require('jsonframe-cheerio')
-let request = require('request')
+const _ = require('lodash')
+
+const cheerio = require('cheerio')
+const jsonframe = require('jsonframe-cheerio')
+const request = require('request')
 
 const frame = {
   'price': 'h3',
@@ -20,77 +22,101 @@ const mapSurface = [0, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 100, 110, 120, 13
 
 // Converts Veasit's search terms into GET params for third-party API
 function convert (search) {
-  let s = {}
-  s.type = (search.search_type === 'buy' ? 'ventes_immobilieres' : 'locations')
+  const s = {}
 
-  // Manage superficy
-  let i = 0
-  if (search.superf_min) {
-    while (search.superf_min > mapSurface[i]) {
-      i++
-    }
-    s.surfacemin = '&sqs=' + i
-  }
-  if (search.superf_max) {
-    i = 0
-    while (search.superf_max > mapSurface[i]) {
-      i++
-    }
-    s.surfacemax = '&sqe=' + i
-  }
-  // On LeBonCoin price is managed differently depending on if you buy or rent
-  if (search.search_type === 'buy') {
-    if (search.price_min) {
-      while (search.price_min > mapPrice[i]) {
+  const promise = new Promise(function (resolve, reject) {
+    s.type = (search.search_type === 'buy' ? 'ventes_immobilieres' : 'locations')
+    s.idtypebien = '&ret=' + (search.house_type === 'house' ? 1 : 2)
+
+    // Manage superficy
+    let i = 0
+    if (search.superf_min) {
+      while (search.superf_min > mapSurface[i]) {
         i++
       }
-      s.pricemin = '&ps=' + i
+      s.surfacemin = '&sqs=' + i
     }
-    if (search.price_max) {
+    if (search.superf_max) {
       i = 0
-      while (search.price_max > mapPrice[i]) {
+      while (search.superf_max > mapSurface[i]) {
         i++
       }
-      s.pricemax = '&pe=' + i
+      s.surfacemax = '&sqe=' + i
     }
-  } else {
-    if (search.price_min) {
-      s.pricemin = '&mrs=' + search.price_min
+    // On LeBonCoin price is managed differently depending on if you buy or rent
+    if (search.search_type === 'buy') {
+      if (search.price_min) {
+        while (search.price_min > mapPrice[i]) {
+          i++
+        }
+        s.pricemin = '&ps=' + i
+      }
+      if (search.price_max) {
+        i = 0
+        while (search.price_max > mapPrice[i]) {
+          i++
+        }
+        s.pricemax = '&pe=' + i
+      }
+    } else {
+      if (search.price_min) {
+        s.pricemin = '&mrs=' + search.price_min
+      }
+      if (search.price_max) {
+        s.pricemax = '&mre=' + search.price_max
+      }
     }
-    if (search.price_max) {
-      s.pricemax = '&mre=' + search.price_max
-    }
-  }
-  return s
+
+    const urlApi = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=correspondance-code-insee-code-postal&q=' + search.cp + '&facet=nom_region'
+    request.get({
+      url: urlApi,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        console.log('Error:', err)
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode)
+      } else {
+        s.region = _.get(data, 'facet_groups[0].facets[0].name').replace(/'|-| /g, '_').toLowerCase()
+        resolve(s)
+      }
+    })
+  })
+  return promise
 }
 
 module.exports = function (search) {
   return new Promise(function (resolve, reject) {
-    let s = convert(search)
+    let s = {}
+    const promise = convert(search)
+    promise.then(function (result) {
+      s = result
 
-    let searchUri = baseUrl + s.type + '/offres/rhone_alpes/?th=1&location=Grenoble%2038000&parrot=0' + s.surfacemin + s.surfacemax + s.pricemin + s.pricemax
+      let searchUri = baseUrl + s.type + '/offres/' + s.region + '/?th=1&location=' + search.cp + '&parrot=0' + s.surfacemin + s.surfacemax + s.pricemin + s.pricemax + s.idtypebien
 
-    console.log('search-leboncoin.js | ' + 'searchUri: ' + searchUri)
+      console.log('search-leboncoin.js | ' + 'searchUri: ' + searchUri)
 
-    let res = []
-    request.get({
-      uri: searchUri,
-      encoding: null
-    }, function (error, response, html) {
-      // First we'll check to make sure no errors occurred when making the request
-      if (!error) {
-        let $ = cheerio.load(html, {
-          normalizeWhitespace: true,
-          xmlMode: false
-        })
-        $('section.tabsContent ul li').each(function (i, elem) {
-          jsonframe($)
-          res[i] = $(this).scrape(frame, { string: false })
-          res[i].date = res[i].date + 'T' + res[i].time.substr(res[i].time.length - 5) + ':00'
-          res[i].price = res[i].price.substr(0, res[i].price.length - 2)
-        })
-        resolve(res)
-      } // endof if !error
-    }) // endof request.get
+      let res = []
+      request.get({
+        uri: searchUri,
+        encoding: null
+      }, function (error, response, html) {
+        // First we'll check to make sure no errors occurred when making the request
+        if (!error) {
+          let $ = cheerio.load(html, {
+            normalizeWhitespace: true,
+            xmlMode: false
+          })
+          $('section.tabsContent ul li').each(function (i, elem) {
+            jsonframe($)
+            res[i] = $(this).scrape(frame, { string: false })
+            res[i].date = res[i].date + 'T' + res[i].time.substr(res[i].time.length - 5) + ':00'
+            res[i].price = res[i].price.substr(0, res[i].price.length - 2)
+          })
+          resolve(res)
+        } // endof if !error
+      }) // endof request.get
+    })
   })
 }
--- routes/search-seloger.js
+++ routes/search-seloger.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const cheerio = require('cheerio')
 const jsonframe = require('jsonframe-cheerio')
 const request = require('request')
@@ -18,28 +20,6 @@ function convert (search) {
   const s = {}
 
   var promise = new Promise(function (resolve, reject) {
-    // do a thing, possibly async, then…
-    var urlApi = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=correspondance-code-insee-code-postal&q=' + search.cp + '&facet=insee_com&facet=nom_dept&facet=nom_region&facet=statut'
-    request.get({
-      url: urlApi,
-      json: true,
-      headers: {'User-Agent': 'request'}
-    }, (err, res, data) => {
-      if (err) {
-        console.log('Error:', err)
-      } else if (res.statusCode !== 200) {
-        console.log('Status:', res.statusCode)
-      } else {
-        resolve(data.facet_groups[0].facets[0].name)
-      }
-    })
-  })
-
-  promise.then(function (r) {
-    console.log('search-seloger.js:39 v')
-
-    s.location = r // "Stuff worked!"
-
     s.idtt = '&idtt=' + (search.search_type === 'buy' ? 2 : 1)
     s.idtypebien = '&idtypebien=' + (search.house_type === 'house' ? 2 : 1)
 
@@ -55,44 +35,60 @@ function convert (search) {
     if (search.superf_max) {
       s.surfacemax = '&surfacemax=' + search.superf_max
     }
-    console.log('search-seloger.js:58 v')
-    console.log(s)
 
-    return s
-  }, function (err) {
-    console.log(err) // Error: "It broke"
+    var urlApi = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=correspondance-code-insee-code-postal&q=' + search.cp + '&facet=insee_com&facet=code_comm'
+    request.get({
+      url: urlApi,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        console.log('Error:', err)
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode)
+      } else {
+        const dep = _.get(data, 'facet_groups[0].facets[0].name[0]') + _.get(data, 'facet_groups[0].facets[0].name[1]')
+        const comm = _.get(data, 'facet_groups[1].facets[0].name')
+        s.location = `?ci=${dep}0${comm}`
+        resolve(s)
+      }
+    })
   })
+
+  return promise
 }
 
 module.exports = function (search) {
   return new Promise(function (resolve, reject) {
-    // TODO ! ! ! s is empty ! ! !
-    const s = convert(search)
+    let s = {}
+    const promise = convert(search)
+    promise.then(function (result) {
+      s = result
 
-    console.log('search-seloger.js:70 v')
-    console.log(s)
+      const searchUri = baseUrl + s.location + s.idtypebien + s.pxmin + s.surfacemin + s.pxmax + s.surfacemax + s.idtt + '&tri=d_dt_crea'
+      console.log('SEARCHURI', searchUri)
 
-    const searchUri = baseUrl + s.location + s.idtypebien + s.pxmin + s.surfacemin + s.pxmax + s.surfacemax + s.idtt + '&tri=d_dt_crea'
-    console.log('search-seloger.js | SEARCHURI', searchUri)
-
-    const annoncesSeLoger = []
-    request.get({
-      uri: searchUri,
-      encoding: null
-    }, function (error, response, html) {
-      // First we'll check to make sure no errors occurred when making the request
-      if (!error) {
-        const $ = cheerio.load(html, {
-          normalizeWhitespace: true,
-          xmlMode: true
-        })
-        $('recherche annonces annonce').each(function (i, elem) {
-          jsonframe($)
-          annoncesSeLoger[i] = $(this).scrape(frame, { string: false })
-          // annoncesSeLoger[i].date = moment(annoncesSeLoger[i].date, 'YYYY-MM-DDThh:mm:ss').fromNow()
-        })
-        resolve(annoncesSeLoger)
-      } // endof if !error
-    }) // endof request.get
+      const annoncesSeLoger = []
+      request.get({
+        uri: searchUri,
+        encoding: null
+      }, function (error, response, html) {
+        // First we'll check to make sure no errors occurred when making the request
+        if (!error) {
+          const $ = cheerio.load(html, {
+            normalizeWhitespace: true,
+            xmlMode: true
+          })
+          $('recherche annonces annonce').each(function (i, elem) {
+            jsonframe($)
+            annoncesSeLoger[i] = $(this).scrape(frame, { string: false })
+            // annoncesSeLoger[i].date = moment(annoncesSeLoger[i].date, 'YYYY-MM-DDThh:mm:ss').fromNow()
+          })
+          resolve(annoncesSeLoger)
+        } // endof if !error
+      }) // endof request.get
+    }, function (err) {
+      console.log(err) // Error: "It broke"
+    })
   })
 }
