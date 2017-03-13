f6ec98956ac757dd7029595cbf133f072e262438
laurentfite <laurent.fite@gmail.com>
2017-03-13 17:16:05 +0100

    Readability improvement for parsing
    - Created a parser module
    - Using jsonframe-cheerio
    - Frames and iconv are selected depending on URL
    - Less 'site-specific' LOC

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,6 @@
 var express = require('express')
 var request = require('request')
-var cheerio = require('cheerio')
-// var jsonframe = require('jsonframe-cheerio')
 var moment = require('moment')
-var extractAddresses = require('address-extractor')
-var extractInfo = require('./info-extractor.js')
-var iconv = require('iconv-lite')
 
 var searchLeboncoin = require('./search-leboncoin.js')
 var searchSeloger = require('./search-seloger.js')
@@ -79,128 +74,19 @@ In order to parse an annonce
 apiRoutes.post('/annonce', function (req, res) {
   const url = req.body.link
 
-  // The structure of our request call
-  // The first parameter is our URL
-  // The callback function takes 3 parameters, an error, response status code and the html
-
   request.get({
     uri: url,
     encoding: null
   }, function (error, response, html) {
-      // First we'll check to make sure no errors occurred when making the request
     if (!error) {
-      var json = { title: '', price: '', desc: '', img: '', superf: '', tel: '', addr: 'ERROR' }
-
-      // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-      if (url.includes('boncoin')) {
-        const frame = {
-          title: '.no-border',
-          price: 'h2.item_price span.value || [0-9| ]+',
-          desc: 'p[itemprop=description]',
-          img: 'div.item_image.big.popin-open.trackable < html || src="(.*?)"',
-          superf: {
-            _s: 'section.properties',
-            _p: /Surface (\d+) m2/
-          }
-        }
-        json = parser(cheerio.load(iconv.decode(html, 'iso-8859-1')), frame)
-      } else if (url.includes('seloger')) {
-        const frame = {
-          title: 'h1',
-          price: '#price || [0-9| ]+',
-          desc: 'p.description',
-          img: 'ul#slider1 < html || src="(.*?)"',
-          superf: {
-            _s: 'ol.description-liste',
-            _p: /Surface de (\d+)/
-          }
-        }
-        json = parser(cheerio.load(html), frame)
-      } else if (url.includes('pap')) {
-        const frame = {
-          title: 'h1',
-          price: '.price || [0-9| ]+',
-          desc: 'p.item-description',
-          img: 'div[data-hash=image0] img@src',
-          superf: {
-            _s: 'ul.item-summary',
-            _p: /Surface (\d+) m/
-          }
-        }
-        json = parser(cheerio.load(iconv.decode(html, 'iso-8859-1')), frame)
-      } else if (url.includes('paruvendu')) {
-        const frame = {
-          title: 'h1.auto2012_dettophead1txt1',
-          price: '#autoprix || [0-9| ]+',
-          desc: '.im12_txt_ann.im12_txt_ann_auto',
-          img: 'div.imdet15-blcphomain < html || src="(.*?)"',
-          superf: {
-            _s: 'ul.imdet15-infoscles',
-            _p: /(\d+)m2/
-          }
-        }
-        json = parser(cheerio.load(iconv.decode(html, 'iso-8859-1')), frame)
-      } else if (url.includes('fnaim38')) {
-        const frame = {
-          title: '',
-          price: '.prix || [0-9| ]+',
-          desc: '.description',
-          img: 'div.imageBig < html || src="(.*?)"',
-          superf: {
-            _s: '.informations',
-            _p: /Surface habitable : (\d+) m/
-          }
-        }
-        json = parser(cheerio.load(html), frame)
-      } else if (url.includes('mobile.avendrealouer')) {
-        const frame = {
-          title: '',
-          price: '.prix || [0-9| ]+',
-          desc: '.description',
-          img: '#slideList < html || src="(.*?)"',
-          superf: {
-            _s: '.details',
-            _p: /(\d+) Surface/
-          }
-        }
-        json = parser(cheerio.load(html), frame)
-      } else if (url.includes('avendrealouer')) {
-        const frame = {
-          title: '',
-          price: '#fd-price-val || [0-9| ]+',
-          desc: '#propertyDesc',
-          img: '.topSummary < html || src="(.*?)"',
-          superf: {
-            _s: '#table',
-            _p: /Surface: (\d+) m/
-          }
-        }
-        json = parser(cheerio.load(html), frame)
-      } else if (url.includes('logic')) {
-        const frame = {
-          title: '',
-          price: 'h2.main-price || [0-9| ]+',
-          desc: '.offer-description-text',
-          img: '#photo < html || src="(.*?)"',
-          superf: {
-            _s: 'span.offer-area-number',
-            _p: /Surface: (\d+) m/
-          }
-        }
-        json = parser(cheerio.load(html), frame)
-      }
-
-      // Finally, we'll define the variables we're going to capture
-      json.link = req.body.link
+      // TODO : change this to res.send(parser(url, html))
+      const json = parser(url, html)
+      console.log(json)
+      res.send(json)
     }
-
-    // Debug
-    console.log(json)
-
-    res.send(json)
   })
 
   return res
-}) // end of app.get(/backend)
+})
 
 module.exports = apiRoutes
